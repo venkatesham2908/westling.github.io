ac96575ca81d776dd5446af4602c46a920d3afd6
venkatesham2908 <104630026+venkatesham2908@users.noreply.github.com>
2023-03-17 01:05:25 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    speed: 500,
+    autoplay: {
+        delay: 5000,
+      },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+  });
